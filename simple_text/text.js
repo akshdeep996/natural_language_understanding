70849d8b7c82ee2ef2f9003b82cb7054191d0d8e
akshdeep996 <rajawatakshdeep@gmail.com>
2018-01-15 03:19:19 +0530

    Improved security

--- simple_text/text.js
+++ simple_text/text.js
@@ -1,13 +1,20 @@
+require('dotenv').config()
+
 var NLUV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 var nlu = new NLUV1({
-  'username': '',   //Your username and password of nlu api
-  'password': '',
+  'username': process.env.username_nlu,
+  'password': process.env.password_nlu,
   'version_date': '2017-02-27'
 });
 
 var letters = {
   'text': 'Nitte Meenakshi is a great colege with great teachers !',
   'features': {
+    'categories':{
+    'art and entertainment':true,
+    'limit':1
+
+   },
     'entities': {
       'emotion': true,
       'sentiment': true,
@@ -18,6 +25,7 @@ var letters = {
       'sentiment': true,
       'limit': 2
     }
+   
   }
 }
 
@@ -25,5 +33,5 @@ nlu.analyze(letters, function(error, result) {
   if (error)
     console.log('OOPS thir was an error which is :', error);
   else
-    console.log(JSON.stringify(result, 0, 4));
+    console.log(JSON.stringify(result, 0, 3));
 });
